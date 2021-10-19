f106784f46d303a9489fbb21ca31da81d9d1ab78
Sula <slt.tmrb@gmail.com>
2021-10-19 16:37:45 +0600

    version7

--- src/components/Footer/indes.jsx
+++ src/components/Footer/indes.jsx
@@ -1,10 +1,16 @@
 import cls from './Footer.module.css'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { AiOutlineFacebook } from 'react-icons/ai'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineYoutube } from 'react-icons/ai'
+import { FaTelegramPlane } from 'react-icons/fa'
+
 
 const Footer = () => {
     return (
         <section className={cls.footer}>
             <div className={cls.footer_conteiner}>
-                <h4>Подписывайтесь на новости</h4>
+                <h4>Подписывайтесь на новости!</h4>
                 <div className={cls.footer_conteiner_input}>
                     <input type='search' placeholder='Введите ваш email'/>
                     <button>Отправить</button>
@@ -13,27 +19,27 @@ const Footer = () => {
                     <ul>
                         <li>
                             <a href='https://myshop-bot422.myinsales.ru/'>
-
+                                <button><AiOutlineInstagram/></button>
                             </a>
                         </li>
                         <li>
                         <a href='https://myshop-bot422.myinsales.ru/'>
-                                
+                                <button><AiOutlineFacebook/></button>
                                 </a>
                         </li>
                         <li>
                         <a href='https://myshop-bot422.myinsales.ru/'>
-                                
+                                <button><AiOutlineTwitter/></button>
                                 </a>
                         </li>
                         <li>
                         <a href='https://myshop-bot422.myinsales.ru/'>
-                                
+                                <button><AiOutlineYoutube/></button>
                                 </a>
                         </li>
                         <li>
                         <a href='https://myshop-bot422.myinsales.ru/'>
-                                
+                                <button><FaTelegramPlane/></button>
                                 </a>
                         </li>
                     </ul>
